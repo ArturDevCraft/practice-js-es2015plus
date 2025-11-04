7998ba0762c3fc6afa793c20e1ebd69bf7c833fb
Artur <artur.czerwiak@gmail.com>
2025-11-04 09:27:04 +0100

    Task 03 completed

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,66 @@
-import {files} from './data.js';
+import { files } from './data.js';
 
+const toByteConvertOperations = {
+	B: (length) => {
+		return length;
+	},
+	KB: (length) => {
+		return length * 1024;
+	},
+	MB: (length) => {
+		return length * 1024 * 1024;
+	},
+	GB: (length) => {
+		return length * 1024 * 1024 * 1024;
+	},
+};
+
+const fromByteConvertOperations = {
+	B: (length) => {
+		return length;
+	},
+	KB: (length) => {
+		return length / 1024;
+	},
+	MB: (length) => {
+		return length / 1024 / 1024;
+	},
+	GB: (length) => {
+		return length / 1024 / 1024 / 1024;
+	},
+};
+
+const recognizeUnit = (bytes) => {
+	if (bytes < 1024) {
+		return 'B';
+	} else if (bytes < 1024 * 1024) {
+		return 'KB';
+	} else if (bytes < 1024 * 1024 * 1024) {
+		return 'MB';
+	} else if (bytes < 1024 * 1024 * 1024 * 1024) {
+		return 'GB';
+	}
+};
+
+const toConvenientUnit = (bytes) => {
+	const unit = recognizeUnit(bytes);
+
+	return { length: fromByteConvertOperations[unit](bytes), unit };
+};
+
+const convertToBytes = (unit = 'B', length) => {
+	return toByteConvertOperations[unit](length);
+};
+
+const calculateTotalSize = (data) => {
+	let totalSize = 0;
+	data.forEach((el) => {
+		const { unit, length } = el.size;
+		const bytes = convertToBytes(unit, length);
+		totalSize += bytes;
+	});
+	return toConvenientUnit(totalSize);
+};
+
+const total = calculateTotalSize(files);
+console.log(total.length + ' ' + total.unit);
